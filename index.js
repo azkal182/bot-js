a4a59f556e8688d2d307514cbacc08dc1f58281e
azkal182 <mohazkalarif@gmail.com>
2022-12-09 22:35:47 +0700

    Set webhooks

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ bot.on('inline_query', async (ctx) => {
   await ctx.answerInlineQuery(result);
 });
 
-bot.launch();
+bot.launch({ webhook: { domain: "https://white-marlin-kit.cyclic.app/", port: 80 } });
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
